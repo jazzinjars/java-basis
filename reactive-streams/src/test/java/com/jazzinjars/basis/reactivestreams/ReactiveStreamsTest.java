3af2172c92fc3c05c25ae87dcd7bfe3844473574
Héctor Ardila López <hectorardila@gmail.com>
2018-07-27 13:27:19 +0200

    Adding ReactiveStreams Tests

---
+++ reactive-streams/src/test/java/com/jazzinjars/basis/reactivestreams/ReactiveStreamsTest.java
@@ -0,0 +1,13 @@
+package com.jazzinjars.basis.reactivestreams;
+
+import org.junit.jupiter.api.Test;
+
+public class ReactiveStreamsTest {
+
+	@Test
+	public void whenSubscribeToIt_thenShouldConsumeAll() throws InterruptedException {
+		//given
+		//when
+		//then
+	}
+}
